560c8c69796720aa342979413ac944eaa0e740c7
emrekececi35 <emrekececi35@gmail.com>
2021-06-22 02:59:18 +0300

    v.0.0.0

---
+++ src/main/java/com/example/game/nn/GameInnApplication.java
@@ -0,0 +1,13 @@
+package com.example.game.nn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameInnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameInnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/game/nn/GameInnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.game.nn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameInnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
