4474870c0a00e61e8aee507f646d13ec97762275
emrekececi35 <emrekececi35@gmail.com>
2021-06-23 13:51:05 +0300

    v.0.0.0

--- src/main/java/com/example/game/nn/repository/UserRepository.java
+++ src/main/java/com/example/game/nn/repository/UserRepository.java
@@ -4,6 +4,6 @@ package com.example.game.nn.repository;
 import com.example.game.nn.Model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository <Integer, User> {
+public interface UserRepository extends JpaRepository <User,Integer> {
 
 }
--- src/main/java/com/example/game/nn/service/UserService.java
+++ src/main/java/com/example/game/nn/service/UserService.java
@@ -2,7 +2,6 @@ package com.example.game.nn.service;
 
 import com.example.game.nn.Model.User;
 import  com.example.game.nn.repository.UserRepository;
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import  org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,9 +11,9 @@ import java.util.Optional;
 public class UserService {
     private final UserRepository userRepository;
 
-    public UserService(UserRepository userRepository){
-        this.userRepository = userRepository;
-    }
+        public UserService(UserRepository userRepository){
+            this.userRepository = userRepository;
+        }
 
         public Optional<User> findUserById(Integer UserId){
             return userRepository.findById(UserId);
